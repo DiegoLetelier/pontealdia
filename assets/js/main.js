8a0fbda2e7de58d9edabdd4857369a358ed79746
Diego Letelier <104950177+DiegoLetelier@users.noreply.github.com>
2022-07-24 20:57:55 -0400

    functions added

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,86 @@ function agregarProducto(producto) {
    let existe = productos.find((elemento) => (elemento.id === producto.id))
    if (!existe) {
       productos.push(producto)
-      localStorage.setItem('misproductos', JSON.stringify(productos))
+      localStorage.setItem('productos', JSON.stringify(productos))
    }
 }
 
 function eliminarProducto(id) {
-   productos = productos.filter((elemento)=>(elemento.id!==id))
-   localStorage.setItem('misproductos', JSON.stringify(productos))
+   productos = productos.filter((elemento) => (elemento.id !== id))
+   localStorage.setItem('productos', JSON.stringify(productos))
    console.log(productos)
-    
+
 }
+
+function obtenerProductos() {
+   let productosEnLS = localStorage.getItem('productos')
+   if (productosEnLS !== null) {
+      productos = JSON.parse(productosEnLS);
+   }
+   console.log(productos)
+
+}
+
+obtenerProductos()
+
+
+// forEach option Editar Productos
+
+function editarProductos(producto) {
+   let { id, nombre, precio, descripcion } = producto;
+   productos.forEach(elemento => {
+      if (elemento.id === id) {
+         elemento.nombre = nombre,
+            elemento.precio = precio,
+            elemento.descripcion = descripcion
+
+      }
+
+   })
+   localStorage.setItem('productos', JSON.stringify(productos))
+   console.log(productos)
+}
+
+
+// index option EditarProductos
+
+function editarProductos2(producto) {
+   // let {id} = producto; --->another option 
+   let index = productos.findIndex(elemento => elemento.id === producto.id);
+   productos.splice(index, 1);
+   agregarProducto(producto)
+   console.log(productos)
+}
+
+// filter option EditarProductos
+
+function editarProductos3(producto) {
+   productos = productos.filter(elemento=>elemento.id !== producto.id)
+   agregarProducto(producto)
+   console.log(productos)
+}
+
+// sort products by number item
+
+function ordenarProductos(){
+   productos.sort((a,b)=>a.precio-b.precio)
+   console.log(productos) 
+}
+
+// sort products on alphabetic order
+
+ function ordenarProductos2(){
+   productos.sort((a,b)=>a.nombre.localeCompare(b.nombre))
+   console.log(productos)
+ }
+
+
+
+
+
+
+
+
 
 
 
